a5c980e23d0d135fabcc5bce4e3a67010faa03c8
twinturbotom <tommmello@gmail.com>
2013-01-03 09:15:40 -0500

    multiple strategies

--- spc.rb
+++ spc.rb
@@ -1,20 +1,36 @@
 require 'csv'
+require 'pp'
+
 f = File.new("sp.csv")
 o = CSV.open('output.csv', 'w')
 
 f.each_line do |l|
 
 	if l !~ /,/
-		
 		#component = l.strip
 		o << [l.strip]
 		#o << [data,f.gets,'off2']
 	elsif l =~ /Quantity/
 		h = l.split(',')
 	else
-		
 		o << ['comma']
 	end
 end
 
 
+
+
+data = File.readlines('sp.csv').slice_before(/^[^,]+$/)
+
+pp data.flat_map { |ary|
+  component = ary[0].strip
+  ary[2..-1].map{ |a|
+    record = CSV.parse(a).flatten
+    [
+      component,
+      record.shift,
+      record.pop,
+      *record[0..-2]
+    ]
+  }
+}
