ae8ba1a8085680d211910460720acc775265e9c4
twinturbotom <tommmello@gmail.com>
2013-01-03 09:19:49 -0500

    outputting data as large array

--- spc.rb
+++ spc.rb
@@ -1,28 +1,10 @@
 require 'csv'
 require 'pp'
 
-f = File.new("sp.csv")
-o = CSV.open('output.csv', 'w')
-
-f.each_line do |l|
-
-	if l !~ /,/
-		#component = l.strip
-		o << [l.strip]
-		#o << [data,f.gets,'off2']
-	elsif l =~ /Quantity/
-		h = l.split(',')
-	else
-		o << ['comma']
-	end
-end
-
-
-
 
 data = File.readlines('sp.csv').slice_before(/^[^,]+$/)
-
-pp data.flat_map { |ary|
+o = CSV.open('output.csv', 'w')
+o << data.flat_map { |ary|
   component = ary[0].strip
   ary[2..-1].map{ |a|
     record = CSV.parse(a).flatten
