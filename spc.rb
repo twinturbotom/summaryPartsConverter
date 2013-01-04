72f4417eb29d49ef2d4678eff668014e5ce5b6c7
twinturbotom <tommmello@gmail.com>
2013-01-03 22:13:02 -0500

    line strategy

--- spc.rb
+++ spc.rb
@@ -1,28 +1,51 @@
 require 'csv'
 
-def parse_sp(file_name)
-  section = []
+file_in = File.new('sp.csv')
+file_out = CSV.open('output.csv', 'w')
 
-  File.foreach(file_name) do |line|
-    case line
-    when /^/
+file_in.each_line do |line|
 
+  case line
+  when /^[^,]+$/
+  header = file_in.gets
+  file_out << [line.strip]
+  end
+  comp_res = line.split(',')
+  file_out << [comp_res[1]]
 
-    end
 
 end
 
 
 =begin
 
-
 require 'csv'
 require 'pp'
 
+f = File.new("sp.csv")
+o = CSV.open('output.csv', 'w')
+
+f.each_line do |l|
+
+  if l !~ /,/
+    #component = l.strip
+    o << [l.strip]
+    #o << [data,f.gets,'off2']
+  elsif l =~ /Quantity/
+    h = l.split(',')
+  else
+    o << ['comma']
+  end
+end
+
+=end
+
+=begin
+
 
 data = File.readlines('sp.csv').slice_before(/^[^,]+$/)
-o = CSV.open('output.csv', 'w')
-o << data.flat_map { |ary|
+
+pp data.flat_map { |ary|
   component = ary[0].strip
   ary[2..-1].map{ |a|
     record = CSV.parse(a).flatten
@@ -34,4 +57,5 @@ o << data.flat_map { |ary|
     ]
   }
 }
+
 =end
