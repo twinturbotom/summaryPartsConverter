dc53a27140bcb6a390f62386c7283703602ebe76
twinturbotom <tommmello@gmail.com>
2013-01-04 11:20:52 -0500

    working on quantity and unit

--- spc.rb
+++ spc.rb
@@ -14,6 +14,7 @@ file_in.each_line do |line|
     next
   when /,/
     vals = line.split(',')
+    #row.push(vals[0],vals[-1]) #adds legend but wrongly
     vals[1..-3].each do |v|
       row.push(v)
     end
