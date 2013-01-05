f7d6b53bdb6d1271f3b9945cd772f9168001abe9
twinturbotom <tommmello@gmail.com>
2013-01-05 14:35:53 -0500

    SP to CSV with comments

--- spc.rb
+++ spc.rb
@@ -3,28 +3,33 @@ require 'csv'
 file_in = File.new('sp1.csv')
 file_out = CSV.open('output.csv', 'w')
 
-header = []
-row = []
+header = [] # First header line of file
+row = [] #Defines a row written to CSV
 
 
 file_in.each_line do |line|
 
   case line
-  when /^[^,]+$/ #Find a component (line with no comma)
-    comp_header = file_in.gets.split(',') #header is after component and is split into an arry
 
-    if header.empty? #header
-      header.push("Component", comp_header[0], comp_header[-1].strip)
-      comp_header[1..-3].each do |h|
+  when /^[^,]+$/ #Find a component (line with no comma)
+    @comp = line.to_s.strip #Sets an instance variable of component
+
+    comp_header = file_in.gets.strip.split(',') #header is after component and is split into an arry
+
+    if header.empty? #if files header array is populated
+      header.push("Component", comp_header[0], comp_header[-1]) #populate first 3 columns of header file  
+      
+      comp_header[1..-3].each do |h| #Append designs to header
         header.push(h)
       end
-      file_out << header 
+    
+    file_out << header #header to file
 
     end
-    @comp = line.to_s.strip
+    
     next
-  when /,/ #when a row had commas
-    puts @comp
+
+  when /,/ #when a row has commas
     vals = line.split(',') #split up into vals array
     row.push(@comp, vals[0], vals[-1].strip) #add quantity and unit to row array
     vals[1..-3].each do |v| #for values (excluding quanity, units, reference info)
